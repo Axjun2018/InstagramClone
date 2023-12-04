f87742ada3d7e3fbc02226a4323644d5a690419a
Wenjun Fan <wfan003@fiu.edu>
2023-12-04 17:51:47 -0500

    change login link color to blue

--- app/src/main/java/com/wenjun/instagramclone/auth/SignupScreen.kt
+++ app/src/main/java/com/wenjun/instagramclone/auth/SignupScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -96,7 +97,7 @@ fun SignupScreen(navController: NavController, vm: IgViewModel){
                 Text(text = "SIGN UP")
             }
             Text(text = "Already a user? Go to login ->",
-                //color = BLUE,
+                color = Color.Blue,
                 modifier = Modifier
                     .padding(8.dp)
                     .clickable {  }
