d996d1cbfc6f2a4344538ef00322daa1e9902f2f
Wenjun Fan <wfan003@fiu.edu>
2023-12-05 21:33:27 -0500

    add user image/icon card

--- app/src/main/java/com/wenjun/instagramclone/main/MainCommonBits.kt
+++ app/src/main/java/com/wenjun/instagramclone/main/MainCommonBits.kt
@@ -4,11 +4,18 @@
 package com.wenjun.instagramclone.main //main package: handle all events in MainActivity
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -17,10 +24,17 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.ImagePainter
+import coil.compose.rememberImagePainter
 import com.wenjun.instagramclone.DestinationScreen
 import com.wenjun.instagramclone.IgViewModel
+import com.wenjun.instagramclone.R
 
 /**
  *  Error handling UI, pop up error notification.
@@ -71,6 +85,52 @@ fun CheckSignedIn(navController: NavController, vm: IgViewModel){
         navController.navigate(DestinationScreen.Feed.route){
             popUpTo(0) //pop all screens to avoid going to login screen
             launchSingleTop = true
+        }
+    }
+}
+
+/**
+ * User post image & User icon UI
+ * pass param to customize
+ */
+@Composable
+fun CommonImage(
+    data: String?, // image Url
+    modifier: Modifier = Modifier.wrapContentSize(),
+    contentScale: ContentScale = ContentScale.Crop //unify image scale/size: Center crop the image into the available space.
+){
+    val painter = rememberImagePainter(data = data)
+    Image(
+        painter = painter,
+        contentDescription = null,
+        modifier = modifier,
+        contentScale = contentScale
+    )
+    if(painter.state is ImagePainter.State.Loading){ // if image is loading, show spinner
+        CommonProgressSpinner()
+    }
+}
+
+/**
+ * User image card
+ * pass param to customize
+ */
+@Composable
+fun UserImageCard(
+    userImage: String?, // nullable, image url
+    modifier: Modifier = Modifier // set default modifier
+        .padding(8.dp)
+        .size(80.dp)
+){
+    Card(shape = CircleShape, modifier = modifier) {
+        if(userImage.isNullOrEmpty()){ // if no user image url, display default icon
+            Image(
+                painter = painterResource(id = R.drawable.ic_user),
+                contentDescription = null,
+                colorFilter = ColorFilter.tint(Color.Gray)
+            )
+        }else{ // otherwise, display commonImage
+            CommonImage(data = userImage)
         }
     }
 }
--- app/src/main/java/com/wenjun/instagramclone/main/MyPostsScreen.kt
+++ app/src/main/java/com/wenjun/instagramclone/main/MyPostsScreen.kt
@@ -1,22 +1,32 @@
 package com.wenjun.instagramclone.main
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.wenjun.instagramclone.IgViewModel
+import com.wenjun.instagramclone.R
 
 @Composable
 fun MyPostsScreen(navController: NavController, vm: IgViewModel){
@@ -26,7 +36,7 @@ fun MyPostsScreen(navController: NavController, vm: IgViewModel){
     Column {
         Column(modifier = Modifier.weight(1f)){
             Row {
-                ProfileImage(userData?.imageUrl){
+                ProfileImage(userData?.imageUrl){ // define trilling lambda in this block
                     
                 }
                 Text(
@@ -84,7 +94,38 @@ fun MyPostsScreen(navController: NavController, vm: IgViewModel){
     }
 }
 
+/**
+ * Profile image for user
+ */
 @Composable
 fun ProfileImage(imageUrl: String?, onClick: () -> Unit){
-    
+    Box(
+        modifier = Modifier
+            .padding(top = 16.dp)
+            .clickable { onClick.invoke() } //onCustomEvent.invoke(): when click profile icon, trigger the passed event function
+    ){
+        // user profile icon
+        UserImageCard(
+            userImage = imageUrl,
+            modifier = Modifier
+                .padding(8.dp)
+                .size(64.dp)
+        )
+        // + icon: to change user icon
+        Card(
+            shape = CircleShape,
+            border = BorderStroke(width = 2.dp, color = Color.White),
+            modifier = Modifier
+                .size(32.dp)
+                .align(Alignment.BottomEnd)
+                .padding(bottom = 8.dp, end = 8.dp)
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.ic_add),
+                contentDescription = null,
+                modifier = Modifier
+                    .background(Color(0xFF495d92))
+            )
+        }
+    }
 }
